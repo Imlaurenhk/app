72d25732fdfe372818289b4a7a0f316d3b109f9b
Lauren Kim <lauren2003kim@gmail.com>
2023-02-11 00:25:55 -0800

    hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 1111;
+
+app.get("/", (req, res) => {
+    res.send("hi");
+});
+
+
+app.get("/fish", (req, res)=> {
+    let favDog = ["chihuahua", "salmon", "pitbull"];
+    res.send(favDog[Math.floor(Math.random() * favDog.length)]);
+});
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+});
